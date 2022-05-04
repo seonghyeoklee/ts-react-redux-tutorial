058416e674d8f796d31fc4d1fa5e7ab2a232df22
woody <woody@woodyui-MacBookPro.local>
2022-05-04 11:13:50 +0900

    setting styled-components

--- src/containers/TodoApp.tsx
+++ src/containers/TodoApp.tsx
@@ -4,6 +4,7 @@ import { RootState } from "../modules";
 import { toggleTodo, removeTodo, addTodo } from "../modules/todos";
 import TodoInsert from "../components/TodoInsert";
 import TodoList from "../components/TodoList";
+import styled from "styled-components";
 
 function TodoApp() {
   const todos = useSelector((state: RootState) => state.todos);
@@ -25,8 +26,18 @@ function TodoApp() {
     <>
       <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
+      <Button>style components 버튼</Button>
     </>
   );
 }
 
 export default TodoApp;
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
